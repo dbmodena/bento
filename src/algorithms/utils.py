d68297016d93164a0736fad8ea7bfc6eb3cf0934
angelomozzillo <117360909+angelomozzillo@users.noreply.github.com>
2024-09-05 18:35:52 +0200

    Update utils.py

--- src/algorithms/utils.py
+++ src/algorithms/utils.py
@@ -14,7 +14,9 @@ from src.datasets.dataset import Dataset
 timestamp = time.strftime("%Y_%m_%d_%H_%M_%S")
 
 
-def save_to_csv(function_name, algo, elapsed_time, memory_usage, ram, pipeline=False, step = False):
+def save_to_csv(
+    function_name, algo, elapsed_time, memory_usage, ram, pipeline=False, step=False
+):
     """Save the execution stats to csv file
 
     Args:
@@ -26,10 +28,8 @@ def save_to_csv(function_name, algo, elapsed_time, memory_usage, ram, pipeline=F
         pipeline (bool, optional): The results are stored in pipeline folder . Defaults to False.
         step (bool, optional): The result are stored in pipeline step folder. Defaults to False.
     """
-    #print("Saving results...")
     if step:
-        #print("Saving Step...")
-        folder=f"{algo.ds_.name}_pipe_step/{algo.name}_mem{algo.mem_}_cpu{algo.cpu_}/"
+        folder = f"{algo.ds_.name}_pipe_step/{algo.name}_mem{algo.mem_}_cpu{algo.cpu_}/"
     elif pipeline:
         folder = f"{algo.ds_.name}_pipe/{algo.name}_mem{algo.mem_}_cpu{algo.cpu_}"
     else:
@@ -57,12 +57,13 @@ def timing(f):
     """
     Decorator that prints the execution time for the decorated function.
     Args:
-        f (function): Function to be decorated 
+        f (function): Function to be decorated
 
     Returns:
         function: Decorated function
     """
     process = psutil.Process(os.getpid())
+
     def wrap(*args, **kwargs):
         # Start the stopwatch
         if args[0].pipeline:
@@ -74,35 +75,28 @@ def timing(f):
             resource.RLIMIT_AS, (resource.RLIM_INFINITY, resource.RLIM_INFINITY)
         )
         tracemalloc.start()
-        
+
         # Call the function
         result = f(*args, **kwargs)
-        # print("Forcing execution...")
         args[0].force_execution()
         memory_used_e = psutil.virtual_memory().used
         print(f"Memory usage: {abs((memory_used_e - memory_used_s))}")
         memory_used = max((memory_used_e - memory_used_s), 0)
         # stopping the library
         tracemalloc.stop()
-        
+
         # Calculate the difference
         ram = max(((tracemalloc.get_traced_memory()[1])), 0)
 
         # Stop the stopwatch and calculate the elapsed time
         end_time = time.time()
         elapsed_time = end_time - start_time
-
-        # Run your method here
-
-        # Stop tracking memory usage and get the memory usage
-        #memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024**2
-
-
         save_to_csv(f.__name__, args[0], elapsed_time, memory_used, ram)
 
         return result
 
     return wrap
+
 
 def compile_json(data, algo: AbstractAlgorithm):
     """
@@ -137,12 +131,6 @@ def get_err_logger():
     Get the error log file
     """
     out_path = os.path.join("results", "errors.log")
-    if not os.path.exists('results'):
-        # create the dir
-        os.makedirs('results')
-        # create the file
-        open(out_path, "wt").close()
-    
     return open(out_path, "at")
 
 
@@ -150,26 +138,24 @@ def set_algorithm_params(args: dict, mem_limit, cpu_limit) -> dict:
     """
     Get the parameters for the algorithm
     """
-    #print(mem_limit, cpu_limit)
     args = json.dumps(args)
     return json.loads(
-        args.replace("MEM_LIMIT", f'{str(mem_limit)}g')
+        args.replace("MEM_LIMIT", f"{str(mem_limit)}g")
         .replace("CPU_LIMIT", str(cpu_limit))
         .replace("PARALLELISM", str(cpu_limit))
     )
 
 
-def execute_methods(methods: dict, ds: Dataset, algo: AbstractAlgorithm, step = False):
+def execute_methods(methods: dict, ds: Dataset, algo: AbstractAlgorithm, step=False):
     """Execute the methods for a specified dataset and algorithm
 
     Args:
         methods (dict): dictionary containing the methods to execute
         ds (Dataset): Dataset to use
         algo (AbstractAlgorithm): Algorithm to use
-        step (bool, optional): Execute pipeline by step, the execution will be forced at the end of every step. 
+        step (bool, optional): Execute pipeline by step, the execution will be forced at the end of every step.
                                Defaults to False.
     """
-    # Open the error log file
     err = get_err_logger()
     for test in methods:
         try:
@@ -180,30 +166,23 @@ def execute_methods(methods: dict, ds: Dataset, algo: AbstractAlgorithm, step = 
             if test["method"] == "load_dataset":
                 # Load the data
                 print("Running " + test["method"])
-                #print(f"Settings: {str(test[input])}")
                 algo.load_dataset(ds, **test[input])
-                #algo.backup()
-                # <#if (not step) and (not algo.pipeline):
-                #     print("Forcing execution...")
-                #     algo.force_execution()
-                #     end = time.time()
-                #     save_to_csv(test["method"], algo, end - start_time, 0, 0)
-                
+
             # skip the force execution step if we execute a pipeline by step
-            elif (not step) and (test["method"]=="force_execution"):
+            elif (not step) and (test["method"] == "force_execution"):
                 continue
             else:
                 print("Running " + test["method"])
                 input_cmd = test[input]
-               
+
                 # Checks if extra commands must be executed before
-                # running the method (e.g. import extra libraries)   
+                # running the method (e.g. import extra libraries)
                 if "pass" in input_cmd:
                     continue
-                
+
                 if "extra_commands" in input_cmd:
                     for cmd in input_cmd["extra_commands"]:
-                        #print(f"Running extra command: {cmd}")
+                        # print(f"Running extra command: {cmd}")
                         exec(cmd, globals())
                     input_cmd.pop("extra_commands")
 
@@ -211,22 +190,17 @@ def execute_methods(methods: dict, ds: Dataset, algo: AbstractAlgorithm, step = 
                 # i.e. replaced with objects from external libraries
                 if "req_compile" in input_cmd:
                     for f in input_cmd["req_compile"]:
-                        #print(f"Running req_compile: {input_cmd[f]}")
                         input_cmd[f] = compile_json(input_cmd[f], algo)
                     input_cmd.pop("req_compile")
-
-                #print(f"Method input: {str(input_cmd)}")
                 getattr(algo, test["method"])(**input_cmd)
-                # if (not step) and (not algo.pipeline):
-                #     print("Forcing execution...")
-                #     algo.force_execution()
-                #     end = time.time()
-                #     save_to_csv(test["method"], algo, end - start_time, 0, 0)
-                if ("restore" in test):
+                if "restore" in test:
                     algo.restore()
         except Exception as e:
             print(
-                colors.color("Cannot execute the method: " + test["method"] + "because" + str(e), fg="red")
+                colors.color(
+                    "Cannot execute the method: " + test["method"] + "because" + str(e),
+                    fg="red",
+                )
             )
             err.write(
                 time.strftime("%Y-%m-%d %H:%M:%S")
@@ -242,7 +216,4 @@ def execute_methods(methods: dict, ds: Dataset, algo: AbstractAlgorithm, step = 
             )
             err.close()
             exit(1)
-            
-    #print("DONE", algo.name)
-    #algo.done()
     err.close()
